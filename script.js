3772734a12ca995786408408e003eaac8f09fc7e
chitrangna <chitrangna.dev@gmail.com>
2026-06-02 20:19:31 +0530

    Comment out existing functions and add new loops

--- script.js
+++ script.js
@@ -1,65 +1,124 @@
-//Answer-1
-function greet(name){
-    console.log(name);
-}
-greet("hello"+" "+"name");
+// //Answer-1
+// // function greet(name){
+// //     console.log(name);
+// // }
+// // greet("hello"+" "+"name");
 
-//Answer-2
-function add(a,b){
-    console.log(a+b);
-}
-add(4,3);
+// // //Answer-2
+// // function add(a,b){
+// //     console.log(a+b);
+// // }
+// // add(4,3);
 
-//Answer-3
-function square(a){
-    console.log(a*a);
-}
-square(5);
+// //Answer-3
+// function square(a){
+//     console.log(a*a);
+// }
+// square(5);
 
-//Answer-4
-function multiply(a,b){
-    console.log(a*b);
-}
-multiply(2,3);
+// //Answer-4
+// function multiply(a,b){
+//     console.log(a*b);
+// }
+// multiply(2,3);
 
-//Answer-5
-function maxoftwo(a,b){
+// //Answer-5
+// function maxoftwo(a,b){
 
-    if(a>b){
-        console.log(a);
-    }
-    else{
-        console.log(b);
-    }
-}
-maxoftwo(7,4);
+//     if(a>b){
+//         console.log(a);
+//     }
+//     else{
+//         console.log(b);
+//     }
+// }
+// maxoftwo(7,4);
 
-//Answer-6
-function kelvin(c){
-    console.log(k=273+c);
-}
-kelvin(4);
+// //Answer-6
+// function kelvin(c){
+//     console.log(k=273+c);
+// }
+// kelvin(4);
 
-//Answer-7
-function even(a){
+// //Answer-7
+// function even(a){
 
-    if(a%2===0){
-        console.log("even");
-    }
-    else{
-        console.log("odd");
-    }
-}
-even(4);
+//     if(a%2===0){
+//         console.log("even");
+//     }
+//     else{
+//         console.log("odd");
+//     }
+// }
+// even(4);
 
-//Answer-8
-function areaofcircle(r){
-    console.log(A=22/7*r*r);
-}
-areaofcircle(4);
+// //Answer-8
+// function areaofcircle(r){
+//     console.log(A=22/7*r*r);
+// }
+// areaofcircle(4);
 
-//Answer-9
-function factorial(n){
-    console.log();
-}
-factorial(5);
+// //Answer-9
+
+
+
+
+// //loops questions
+// // 1 se 10 tk numbers print karo
+
+// for (let i=1; i<11; i++){
+//     console.log(i);
+
+// }
+// let i=1;
+// do{
+//     console.log(i);
+//     i++
+// }while(i<9);
+
+// while(i<9){
+//     console.log(i);
+//     i++
+// }
+// //doubt ki kya let waki k liye bhi kam krega ya nhi
+
+
+
+// //10 se 1 tk reverse counting print karo
+// for (let i=10; i>1; i-- ){
+//     console.log(i);
+// }
+
+// //1 se 100 tk even number print karo
+// for (let i=1; i<40; i++){
+
+//     if(i%2===0){
+//         console.log(i);
+
+//     }
+
+// }
+
+// //1 se 100 tk odd number print karo 
+// for (let i=0; i<40; i++){
+
+//     if(i%2!==0){
+//         console.log(i);
+//     }
+
+// }
+
+//number ka table print karo
+ for (let i=5; i<51; i+=5 ){
+     console.log(i);
+
+ }
+
+ // 1se n tk numbers ka sum nikalo
+ let sum=0;
+ for (let i=1; i<n; i++ ){
+    console.log(i+i);
+
+ }
+
+ 
